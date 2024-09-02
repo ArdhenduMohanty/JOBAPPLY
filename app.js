6eca85c9e73b331220dda49eeed9fedb74b92bfc
ArdhenduMohanty <123829652+ArdhenduMohanty@users.noreply.github.com>
2024-09-02 12:08:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ import { newsLetterCron } from "./automation/newsLetterCron.js";
 const app = express();
 config({ path: "./config/config.env" });
 
-app.use(cors());
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }));
 
 app.use(cookieParser());
 app.use(express.json());
